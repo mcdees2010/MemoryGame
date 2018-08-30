18bb9877c7ef28027637a9c8e724eeef70c5429d
Maria Dees <mcdees2010@gmail.com>
2018-08-30 16:26:42 -0700

    game fully working

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ This Memory game is a card game that challenges the user to improve their memory
 
 Your task is to match three sets of cards. the rest of the cards are random, and after you get three matches you win the game!
 
+<img src="https://i.imgur.com/o1yxHei.png">
+
+<img src="https://imgur.com/T9l5Izy.png">
+
+### Technologies Used
+
+1) HTML<br>
+
+2) CSS<br>
+
+3) Javascript
--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 const gameArea = document.querySelector("#game");
 const cards = document.querySelectorAll('.card');
 const btn = document.getElementById("start-game");
-const score = document.getElementById("score");
+
 
 /*
  * GAME STATE
@@ -16,7 +16,6 @@ const numbers = ["J", "Q", "K", "A"];
 let matches = [];
 let currentPair = [];
 let intervalId, memoryCards, deck;
-let points = 0;
 
 
 function createDeck() {
@@ -80,12 +79,8 @@ function hideCards(cards) {
 };
 
 function showCard(evt) {
-
     if (currentPair.length > 1) return;
     let id = parseInt(evt.target.getAttribute('key'));    
-    console.log(id);
-    console.log(matches);
-    if (memoryCards[id].clicked || matches.includes(id)) return;
 
     // Mark the card as clicked
     memoryCards[id].clicked = true;
@@ -110,24 +105,20 @@ function checkWin(){
 }
 
 function loseGame() {
-    setMessage("LOSER! Try again!");
+    setMessage("Game Over! Try again.");
     resetGame();
 }
 
 function checkMatch() {
-    // Check if current pair is greater than 2.
     if (currentPair.length < 2) return;
-    
     let card1 = currentPair[0].classList[1];
     let card2 = currentPair[1].classList[1];
     if (card1 === card2) {
-        points++;
         matches.push(currentPair[0]);
         matches.push(currentPair[1]);
     } else {
         hideCards(currentPair);
     }
-
     currentPair = [];
 }
 
@@ -138,11 +129,6 @@ function resetGame(){
     hideCards(matches);
     matches = [];
     createCards();
-    // setTimeout(function() {
-    //     setMessage("Ready to Play!");
-    // }, 3000);
-    //reshuffle cards
-    // reset the state
 }
 
 function shuffleCards(deck) {
@@ -154,63 +140,3 @@ function shuffleCards(deck) {
     }
     return deck;
 }
-
-
-
-
-//reset game variables on win/lose
-
-// function checkMatch() {
-//     let currentCard = currentPair.shift();
-//     let id = parseInt(currentCard.id.split("-")[1]);
-//     memoryCards[id].clicked = false;
-//     if (thisCard === lastCard) {
-//         matches.push(thisCard);
-//         return;
-//         console.log('Winner');
-//     }
-//     hideCard(currentCard, currentCard.classList[1]);
-//     if (matches.length === 6){
-//         alert('WON GAME')
-
-//     }
-// }
-
-
-
-// function endGame(){
-//     if(currentPair === 3){
-//        alert("You won the game!")
-//     }else {
-//        alert("Game Over!, try again.")
-//        clearInterval();
-// }
-// } need to add it within the time limit of the countdown!
-//   if timer = 0 && currentPair === 3
-
-// function endGame(){
-//     if there are 3 matches then game is over! 
-// no more contiunuing to play
-//   if(matches > 3){
-//    endGame 
-//    reset board with reset button
-// alert/pop up that the game has been won
-
-    // let lastElem = currentPair[currentPair.length-1];
-
-    // if (currentCard === lastElem) {
-    //     points++;
-    // }
-    // if (currentPair.length === 2) {
-    //     let cardOnevalue = currentPair[0].classList[1]
-    //     let cardTwovalue = currentPair[1].classList[1]
-    //     if (cardOnevalue === cardTwovalue) {
-    //         points++;
-    //         currentPair = [];
-    //         alert("you got a pair!");
-    //     } else {
-    //         hideCard(currentPair[0], cardOnevalue);
-    //         hideCard(currentPair[1], cardTwovalue);
-    //         currentPair = [];
-    //     }
-    // }
